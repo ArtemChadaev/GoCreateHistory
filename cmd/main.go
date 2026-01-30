fd115b42f500a14fd6e2903fd4431574606100cd
ArtemChadaev <030806chadaevartem@gmail.com>
2026-01-30 11:38:40 +0300

    Сделал нормальный main для проверки

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,53 @@
 package main
 
-import "github.com/ArtemChadaev/GoCreateHistory/internal/config"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ArtemChadaev/GoCreateHistory/internal/config"
+	"github.com/ArtemChadaev/GoCreateHistory/internal/domain"
+	"github.com/ArtemChadaev/GoCreateHistory/internal/handler"
+	"github.com/ArtemChadaev/GoCreateHistory/internal/repository"
+	"github.com/ArtemChadaev/GoCreateHistory/internal/service"
+)
 
 func main() {
-	_, err := config.Load()
+	cfg, err := config.Load()
 
 	if err != nil {
-
+		log.Fatal(err)
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// 1. Инициализация ресурсов (БД и Redis)
+	db, err := repository.NewPostgresDB(repository.PostgresConfig{
+		Host:     cfg.DBHost,
+		Port:     cfg.DBPort,
+		Username: cfg.DBUser,
+		Database: cfg.DBName,
+		Password: cfg.DBPassword,
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(domain.Server)
+	go func() {
+		if err := srv.Run(cfg.Port, handlers.Routes()); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("HTTP server error: %v", err)
+		}
+	}()
+	<-ctx.Done()
 }
